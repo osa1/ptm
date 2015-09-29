0a747d58c7205346dca00f29e61291c9f1e8a329
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-09-29 13:59:05 -0400

    factor out parse-print-parse test code

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -5,11 +5,11 @@ import qualified Language.Haskell.Exts as HSE
 import System.Directory
 import System.FilePath
 
+import Prelude hiding (print)
+
 import Test.Hspec
 import Test.Hspec.Contrib.HUnit
 import Test.HUnit hiding (path)
--- I hate it when libraries export unqualified record field selectors with super
--- generic names, like path, ret, result, etc.
 
 import CoreLike.Parser
 import CoreLike.ToHSE
@@ -23,23 +23,33 @@ main = hspec spec
 spec :: Spec
 spec = do
     files <- runIO $ loadTestFiles "tests"
+
     describe "parsing and printing" $ forM_ files $ \(path, contents) ->
       fromHUnitTest $ TestLabel ("parsing and printing " ++ path) $ TestCase $
-        case parseModule contents of
-          Left err -> assertFailure $ "Parsing failed: " ++ err
-          Right bs ->
-            let printed = HSE.prettyPrint $ hseModule bs in
-            case parseModule printed of
-              Left err -> assertFailure $ unlines
-                [ "Can't parse printed module:", printed,
-                  "printed AST:", showPretty bs,
-                  "error:", err ]
-              Right bs' ->
-                assertEqStrs
-                  (unlines [ "Printed module is parsed differently",
-                             "printed:", printed ])
-                  bs bs'
-                  (showPretty bs) (showPretty bs')
+        ppp contents parseModule (HSE.prettyPrint . hseModule)
+
+-- | A parse-print-parse assertion.
+ppp :: (Show a, Eq a)
+    => String                      -- ^ string to ppp
+    -> (String -> Either String a) -- ^ parser
+    -> (a -> String)               -- ^ printer
+    -> Assertion
+ppp s parse print = do
+    case parse s of
+      Left err -> assertFailure $ "Parsing failed: " ++ err
+      Right a  ->
+        let printed = print a in
+        case parse printed of
+          Left err -> assertFailure $ unlines
+            [ "Can't parse printed module:", printed,
+              "printed AST:", showPretty printed,
+              "error:", err ]
+          Right a' ->
+            assertEqStrs
+              (unlines [ "Printed module is parsed differently",
+                         "printed:", printed ])
+              a a'
+              (showPretty a) (showPretty a')
 
 loadTestFiles :: FilePath -> IO [(FilePath, String)]
 loadTestFiles root = getDirectoryContents root >>= fmap concat . mapM f
